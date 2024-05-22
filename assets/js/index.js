8b7b1f83dfa793eef2f4723609d8b7e294db1c66
Jefferson Fonseca <arielfonsek5@gmail.com>
2024-05-22 15:21:35 -0300

    att exclusão do anexo e do numero do pedido

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,13 @@ function sendMail() {
   var email = document.getElementById("email").value;
   var cpf = document.getElementById("cpf").value;
   var phone = document.getElementById("phone").value;
-  var pedido = document.getElementById("pedido").value;
+  //var pedido = document.getElementById("pedido").value;
   var produto = document.getElementById("produto").value;
   var cor = document.getElementById("cor").value;
   var peça = document.getElementById("peça").value;
   var tipoProduto = document.getElementById("tipoProduto").value;
   var detalhes = document.getElementById("detalhes").value;
-  var attachments = document.getElementById("attachments").files;
+  //var attachments = document.getElementById("attachments").files;
 
   // Verifica se todos os campos obrigatórios estão preenchidos
   if (!name || !email || !cpf || !pedido || !produto || !cor || !peça || !tipoProduto || !detalhes) {
@@ -22,13 +22,13 @@ function sendMail() {
       email: email,
       cpf: cpf,
       phone: phone,
-      pedido: pedido,
+      //pedido: pedido,
       produto: produto,
       cor: cor,
       peça: peça,
       tipoProduto: tipoProduto,
       detalhes: detalhes,
-      attachments: attachments
+      //attachments: attachments
   };
 //aqui estão as apis para envio
   const serviceID = "service_20rl9zg";
